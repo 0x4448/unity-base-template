3536f15acdf287588ea6f85a940ada96c40404ce
0x4448 <86135470+0x4448@users.noreply.github.com>
2022-10-03 00:45:07 +0000

    Add basic SceneLoader class

--- Assets/Scripts/Managers/SceneLoader/GameScene.cs
+++ Assets/Scripts/Managers/SceneLoader/GameScene.cs
@@ -1,8 +1,9 @@
 /// <summary>
-/// All scenes in build settings.
+/// Scene in build settings.
 /// </summary>
 public enum GameScene
 {
+    // All scenes in build settings must be listed in order.
     Startup,
     PersistentManagers,
 }
--- Assets/Scripts/Managers/SceneLoader/GameSceneChannel.cs
+++ Assets/Scripts/Managers/SceneLoader/GameSceneChannel.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewGameSceneEvent", menuName = "Event Channels/Game Scene Event")]
 public class GameSceneEventChannel : ScriptableObject
 {
-    public delegate void GameSceneEvent(GameObject sender, GameScene index);
+    public delegate void GameSceneEvent(GameObject sender, GameScene scene);
     public event GameSceneEvent Handler;
 
     /// <summary>
     /// Ask the SceneLoader to load or unload a scene.
     /// </summary>
     /// <param name="sender">The GameObject that raised the event.</param>
-    /// <param name="index">Index of the scene in Build Settings.</param>
-    public void Raise(GameObject sender, GameScene index) => Handler?.Invoke(sender, index);
+    /// <param name="scene">Index of the scene in Build Settings.</param>
+    public void Raise(GameObject sender, GameScene scene) => Handler?.Invoke(sender, scene);
 
     /// <inheritdoc cref="Raise(GameObject, GameScene)"/>
-    public void Raise(GameScene index) => Raise(null, index);
+    public void Raise(GameScene scene) => Raise(null, scene);
 }
